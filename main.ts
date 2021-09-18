11116be2ba6a0d035ab1e4a17f86e829282a7604
ezziechai <85370053+ezziechai@users.noreply.github.com>
2021-09-18 11:02:06 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,327 @@
+namespace SpriteKind {
+    export const Grocery = SpriteKind.create()
+    export const CartItem = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Grocery, SpriteKind.Grocery, function (sprite, otherSprite) {
+    tiles.placeOnRandomTile(sprite, assets.tile`tile1`)
+})
+function createTextSprite () {
+    subTotalSprite = textsprite.create("$0")
+    subTotalSprite.left = 0
+    subTotalSprite.top = 0
+    subTotalSprite.setFlag(SpriteFlag.RelativeToCamera, true)
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile10`, function (sprite, location) {
+    display = "Subtotal: $" + subtotal
+    for (let item of sprites.allOfKind(SpriteKind.CartItem)) {
+        let cost = sprites.readDataNumber(item, "cost")
+        let name = sprites.readDataString(item, "name")
+    display = "" + display + "\n" + name + ": $" + cost
+    }
+    game.showLongText(display, DialogLayout.Center)
+    info.setScore(subtotal)
+    game.over(true)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Grocery, function (p, g) {
+    if (controller.A.isPressed()) {
+        addToCart(g)
+        pause(100)
+    }
+})
+function addToCart (grocery: Sprite) {
+    item2 = sprites.create(grocery.image, SpriteKind.CartItem)
+    item2.follow(sierra)
+    item2.x = sierra.x
+    item2.y = sierra.y
+    cost = sprites.readDataNumber(grocery, "cost")
+    subtotal += cost
+    subTotalSprite.setText("$" + subtotal)
+    weight = sprites.readDataNumber(grocery, "weight")
+    speed += 0 - weight
+    if (speed < 5) {
+        speed = 5
+    }
+    controller.moveSprite(sierra, speed, speed)
+    name = sprites.readDataString(grocery, "name")
+    sprites.setDataNumber(item2, "cost", cost)
+    sprites.setDataString(item2, "name", name)
+}
+function createProduct (productImg: Image, cost: number, weight: number, name: string) {
+    p = sprites.create(productImg, SpriteKind.Grocery)
+    sprites.setDataNumber(p, "cost", cost)
+    sprites.setDataNumber(p, "weight", weight)
+    sprites.setDataString(p, "name", name)
+    tiles.placeOnRandomTile(p, assets.tile`tile1`)
+}
+function createAllProducts () {
+    for (let i = 0; i <= groceryImages.length - 1; i++) {
+        createProduct(groceryImages[i], groceryCosts[i], groceryWeights[i], groceryNames[i])
+    }
+}
+let p: Sprite = null
+let name = ""
+let weight = 0
+let item2: Sprite = null
+let cost = 0
+let subtotal = 0
+let display = ""
+let subTotalSprite: TextSprite = null
+let sierra: Sprite = null
+let groceryCosts: number[] = []
+let groceryWeights: number[] = []
+let groceryNames: string[] = []
+let groceryImages: Image[] = []
+let speed = 0
+let weight2 = 0
+let cost2 = 0
+let image2 = null
+speed = 100
+groceryImages = [
+img`
+    . . . 2 2 2 . . . . . . . . . . 
+    . . . c c c 6 6 8 8 . . . . . . 
+    . . 6 e e e e e c 6 8 . . . . . 
+    . 6 e e e e e e 8 c 6 8 . . . . 
+    6 e e e e e e 8 . 8 c 8 . . . . 
+    6 e e e e e e 8 . 8 c 8 . . . . 
+    8 c e e e e e 8 . 8 c 8 . . . . 
+    8 c e e e e e 8 8 c c 8 . . . . 
+    8 c c c c c c c c c c 8 . . . . 
+    8 6 c c c c c c c c c 8 . . . . 
+    . 8 6 c c c c c c c 6 8 . . . . 
+    . . 8 8 8 8 8 8 8 8 8 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . 2 2 2 . . . . . . . . . . 
+    . . . c c c 6 6 8 8 . . . . . . 
+    . . 6 1 1 1 1 1 9 6 8 . . . . . 
+    . 6 1 1 1 1 1 1 8 9 6 8 . . . . 
+    6 1 1 1 1 1 1 8 . 8 9 8 . . . . 
+    6 1 1 1 1 1 1 8 . 8 9 8 . . . . 
+    8 9 1 1 1 1 1 8 . 8 9 8 . . . . 
+    8 9 1 1 1 1 1 8 8 9 9 8 . . . . 
+    8 9 9 9 9 9 9 9 9 9 9 8 . . . . 
+    8 6 9 9 9 9 9 9 9 9 9 8 . . . . 
+    . 8 6 9 9 9 9 9 9 9 6 8 . . . . 
+    . . 8 8 8 8 8 8 8 8 8 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . 6 6 6 . . . . . . 
+    . . . . . . . c b c . . . . . . 
+    . . . . . c c c b c c c . . . . 
+    . . . . c b b b b b b b c . . . 
+    . . . . c 1 b b b b b 1 c . . . 
+    . . . . c 1 1 7 1 7 1 1 c . . . 
+    . . . . c 1 1 1 7 1 1 1 c . . . 
+    . . . . c 1 1 a c a 1 1 c . . . 
+    . . . . c 1 a c a c a 1 c . . . 
+    . . . . c 1 c a c a c 1 c . . . 
+    . . . . c 1 a c a c a 1 c . . . 
+    . . . . c 1 c a c a 1 1 c . . . 
+    . . . . c 1 a c a 1 1 1 c . . . 
+    . . . . c b 1 a 1 1 1 b c . . . 
+    . . . . c b b b b b b b c . . . 
+    . . . . . c c c c c c c . . . . 
+    `,
+img`
+    . c c c c c c c c c c c c c . . 
+    c b b b b b b b b b b b b b c . 
+    c b b b b b b b b b b b b b c . 
+    c c c c c c c c c c c c c c c . 
+    c d d 1 1 1 1 1 1 1 1 1 d d c . 
+    c d c c c 1 c c c 1 c c c d c . 
+    c d c 1 c 1 c 1 c 1 1 c d d c . 
+    c d c 1 c 1 c c c 1 1 c d d c . 
+    c d c c c 1 c 1 c 1 1 c d d c . 
+    c d d 1 1 1 1 1 1 1 1 1 d d c . 
+    c d d 1 1 1 2 2 2 1 1 1 d d c . 
+    c d d 1 1 2 8 8 8 2 1 1 d d c . 
+    c d d 1 1 2 8 d 8 2 1 1 d d c . 
+    c d d 1 1 2 8 6 8 2 1 1 d d c . 
+    . c d 1 1 1 2 2 2 1 1 1 d c . . 
+    . . c c c c c c c c c c c . . . 
+    `,
+img`
+    . . . c c c c . . . . . . . . . 
+    . . c e e e e c c c . . . . . . 
+    . c e e e e e e e e c . . . . . 
+    . c e e e e e e e e e c . . . . 
+    f e e e e c c c e e e c . b b . 
+    f e e e c e e e c c c c c d d b 
+    f c e e f e e e e e e e c d d b 
+    f c c c f e e e f f f f c b b b 
+    f c c c c f f f c c c f . c c . 
+    . f c c c c c c c c c f . . . . 
+    . f c c c c c c c c f . . . . . 
+    . . f f f f f f f f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . 6 6 9 9 9 9 . . . . . . . . 
+    . 6 9 9 1 1 1 1 9 . . . . . . . 
+    6 9 6 9 9 9 1 9 1 9 . . . . . . 
+    6 9 9 6 6 6 6 1 1 9 . . . . . . 
+    6 9 9 6 9 9 6 1 1 9 . . . . . . 
+    . 6 9 6 9 9 6 1 9 . . . . . . . 
+    . . 6 9 6 6 1 9 . . . . . . . . 
+    . . . 6 9 9 9 . . . . . . . . . 
+    . . . . 6 6 . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . c c c c c c c . . . . . 
+    . . . c d d d d d d d c . . . . 
+    . . c d d d d d b b d d c . . . 
+    . . b c d d d d d d d c b . . . 
+    . . b 2 c c c c c c c 2 b . . . 
+    . . b 2 2 2 2 2 2 2 2 2 b . . . 
+    . . b 2 2 2 2 2 2 2 2 2 b . . . 
+    . . b 2 2 2 b b b 2 2 2 b . . . 
+    . . b 2 2 b 2 2 2 b 2 2 b . . . 
+    . . d 1 2 b 2 2 2 b 2 1 d . . . 
+    . . d 1 1 b 2 2 2 b 1 1 d . . . 
+    . . d 1 1 b 2 2 2 b 1 1 d . . . 
+    . . d 1 1 1 b b b 1 1 1 d . . . 
+    . . . d 1 1 1 1 1 1 1 d . . . . 
+    . . . . d d d d d d d . . . . . 
+    `,
+img`
+    . . c c c c c c c c c c . . . . 
+    . c d d d d d d d c b b c . . . 
+    c d d d d d d d c b d b b c . . 
+    c c d d d d d d d c b b c c . . 
+    c b c c c c c c c c c c b c . . 
+    c b 8 9 8 b 8 9 9 9 8 b b c . . 
+    c b b 8 9 6 9 6 9 6 9 8 b c . . 
+    c b b 8 9 6 9 6 9 6 9 8 b c . . 
+    c b 8 9 8 b 8 9 9 9 8 b b c . . 
+    . c c c c c c c c c c c c . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . b 1 1 1 1 1 1 1 1 1 . . . 
+    . . b 1 1 1 1 1 1 1 1 1 1 1 . . 
+    . . b 1 1 1 1 1 1 1 1 1 8 8 . . 
+    . . b 1 1 1 1 1 1 1 8 8 8 8 . . 
+    . . b 1 1 1 5 5 5 5 8 8 8 8 . . 
+    . . b 1 1 5 5 5 5 5 5 8 8 8 . . 
+    . . b 1 8 5 5 5 5 5 5 8 8 8 . . 
+    . . c 8 8 5 5 5 5 5 5 8 1 1 . . 
+    . . c 8 8 5 5 5 5 5 5 1 1 1 . . 
+    . . c 8 8 8 5 5 5 5 1 1 1 1 . . 
+    . . c 8 8 8 1 1 1 1 1 1 1 1 . . 
+    . . c 2 2 2 1 1 1 1 6 6 6 1 . . 
+    . . b 1 2 1 1 1 1 1 1 1 1 1 . . 
+    . . b 1 1 1 1 1 1 1 1 1 1 1 . . 
+    . . . b b b b b b b b b b . . . 
+    `,
+img`
+    . . . . . . . 6 . . . . . . . . 
+    . . . . 6 6 6 6 6 6 6 . . . . . 
+    . . 6 6 6 6 7 6 7 6 6 6 6 . . . 
+    . 6 6 7 6 6 7 6 7 6 6 7 7 6 . . 
+    . 6 6 7 6 7 6 6 7 6 6 7 6 6 . . 
+    6 7 6 7 6 7 6 6 7 7 6 7 6 7 6 . 
+    6 7 6 7 6 7 6 6 7 7 6 7 6 7 6 . 
+    6 7 6 7 6 7 6 6 7 7 6 7 6 7 6 . 
+    6 7 6 7 6 7 6 6 7 7 6 7 6 7 6 . 
+    6 7 6 7 6 7 6 6 7 7 6 7 6 7 6 . 
+    6 7 6 7 6 7 6 6 7 6 6 7 6 7 6 . 
+    6 6 6 7 6 7 6 6 7 6 6 7 6 6 6 . 
+    . 6 6 7 6 7 6 6 7 6 6 7 6 6 . . 
+    . 6 6 7 6 7 7 6 7 6 6 7 6 6 . . 
+    . . 6 6 6 6 7 6 7 6 6 6 6 . . . 
+    . . . . 6 6 6 6 6 6 6 . . . . . 
+    `
+]
+groceryNames = [
+"Chocolate Milk",
+"Milk",
+"Grape Soda",
+"Oatmeal",
+"Turkey",
+"Fancy glass",
+"Chicken soup",
+"Sardines",
+"Flour",
+"Watermelon"
+]
+groceryWeights = [
+8,
+8,
+2,
+1,
+12,
+0.5,
+0.5,
+0.5,
+5,
+10
+]
+groceryCosts = [
+3,
+2,
+3,
+4,
+20,
+10,
+2,
+1,
+5,
+3
+]
+scene.setBackgroundColor(9)
+tiles.setTilemap(tilemap`level`)
+sierra = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+sierra.setPosition(7, 60)
+scene.cameraFollowSprite(sierra)
+game.showLongText("Hello! I'm Sierra.", DialogLayout.Bottom)
+game.showLongText("You may not know this, but I have to go shopping.", DialogLayout.Bottom)
+game.showLongText("BTW, I don't have a shopping cart.", DialogLayout.Bottom)
+game.showLongText("Use the arrow keys to move me and A to make me pick things up.", DialogLayout.Bottom)
+game.showLongText("Good luck!", DialogLayout.Bottom)
+controller.moveSprite(sierra, speed, speed)
+createAllProducts()
+createTextSprite()
+info.startCountdown(30)
